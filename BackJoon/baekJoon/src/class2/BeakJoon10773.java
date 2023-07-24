28ddc45a4204f160c4953e6c3f401216cd8d67d2
programofktw <thkim000317@naver.com>
2023-07-24 13:56:17 +0900

    2023_07_24 BaekJoon 10773 solved

--- BackJoon/baekJoon/src/class2/BeakJoon10773.java
+++ BackJoon/baekJoon/src/class2/BeakJoon10773.java
@@ -1,5 +1,31 @@
 package class2;
 
-public class BeakJoon10773 {
+import java.util.Scanner;
+import java.util.Stack;
 
+public class BeakJoon10773 {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		
+		Stack<Integer> stack = new Stack<Integer>();
+	
+		int N = scan.nextInt();
+		
+		int input;
+		
+		int sum = 0;
+		
+		for(int i = 0 ; i<N;i++) {
+			input = scan.nextInt();
+			
+			if(input == 0 ) stack.pop();
+			else stack.add(input);
+		}
+		
+		while(!stack.isEmpty()) {
+			sum+= stack.pop();
+		}
+		
+		System.out.println(sum);
+	}
 }
